03420da6cfcf37ccbcd9673d1b7ad17eca58d68c
Kevin <kl29@princeton.edu>
2020-05-27 12:00:53 -0500

    commit7

---
+++ build/precache-manifest.5351774cb4d904673bda0b8872d88795.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a3a39094be016f54b58cebe07272bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ff8713033d80b7e9e71",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff8713033d80b7e9e71",
+    "url": "/static/js/main.43b7b1b3.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
