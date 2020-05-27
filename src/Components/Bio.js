e27a9abb24bc8021c24c0ee523af29a199e7e584
Kevin <kl29@princeton.edu>
2020-05-27 11:38:41 -0500

    git add --all

---
+++ src/Components/Bio.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+function Bio(props) {
+    return (
+    <div style={{textAlign:"left"}}>
+        <p>
+            Princeton Computer Science BSE 2022<br/>
+            <a style={{color:"#99ffff"}} href="https://github.com/kl29" target="_blank">Visit my Github!</a>
+            <br/><br/>
+
+            Fluent Technologies: 
+            <ul style={{textAlign:"left"}}>
+                <li>Python (Django + Databases, Colab)</li>
+                <li>Java (Algorithms, Data Structures)</li>
+                <li>C (Systems)</li>
+                <li>Javascript (React)</li>
+                <li>R (functional, Data Science)</li>
+
+            </ul>
+    
+            <hr/>
+            Here are my projects:
+        </p>
+    </div>
+    )
+}
+
+export default Bio
---
+++ src/Components/Projects.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Projects(props) {
+    return (
+    <div>
+        <h3>
+            <a style={{color:"#99ffff"}} 
+                href="https://github.com/kl29/NextLevel-Labs-MVP-site-front-end/blob/master/index.php" target="blank">
+                HTML Front-end Website 
+            </a>
+        </h3>
+    </div>
+    )
+}
+
+export default Projects
